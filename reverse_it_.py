6e1a337ac76ec3fee2a8376b7a5e4b8a18fc0fc3
Vartem4got <artwitch3000@gmail.com>
2024-10-07 20:45:08 +0300

    Add files via upload

---
+++ reverse_it_.py
@@ -0,0 +1,17 @@
+
+
+
+
+def rvrs_str(inpt_str):
+  rvrs_str = ""
+  for i in range(len(inpt_str) - 1, -1, -1):
+    rvrs_str += inpt_str[i]
+  return rvrs_str
+
+
+inpt_str = input("Enter your str - ")
+
+
+rslt = rvrs_str(inpt_str)
+
+print(f"reversed str - {rslt}")
