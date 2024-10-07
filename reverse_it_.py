2ab7be4613fb01cd75beb500c80974504947189e
Vartem4got <artwitch3000@gmail.com>
2024-10-07 20:45:32 +0300

    reverse_it_.py

--- reverse_it_.py
+++ reverse_it_.py
@@ -1,7 +1,5 @@
 
 
-
-
 def rvrs_str(inpt_str):
   rvrs_str = ""
   for i in range(len(inpt_str) - 1, -1, -1):
@@ -15,3 +13,4 @@ inpt_str = input("Enter your str - ")
 rslt = rvrs_str(inpt_str)
 
 print(f"reversed str - {rslt}")
+
